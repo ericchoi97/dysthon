dec91b275597f3d40a87dc1d075ed18b27845aba
Eric Choi <lastappstanding@gmail.com>
2023-06-08 23:36:48 -0400

    Initial
    A Python library with Dysthon syntax translations for improved readability and accessibility.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # dysthon
  A Python syntax library for coders with dyslexia.
+
+ You can then import the library into your Python code using the following syntax:
+
+ from dysthon_lib import *
+
+ After importing, you will have access to the translated Dysthon commands for each corresponding Python command. For example, you can use during instead of while, attach instead of append, words instead of dictionary, and so on.
---
+++ dysthon_lib.py
@@ -0,0 +1,238 @@
+# File: dysthon_lib.py
+
+# Control Flow
+during = while
+unless = except
+
+# List Operations
+attach = append
+
+# Dictionary Operations
+words = dictionary
+
+# Loop Iteration
+count = enumerate
+
+# Assertions
+check = assert
+
+# Code Execution
+run = exec
+
+# Superclass
+parent = super
+
+# Filtering
+sort = filter
+
+# Decorators
+decorate = decorator
+
+# Iterators
+cursor = iterator
+
+# Multiprocessing
+multi = multiprocessing
+
+# Threading
+thread = threading
+
+# Queue
+line = queue
+
+# Namespaces
+scope = namespace
+
+# Interfaces
+bridge = interface
+
+# Encoding and Decoding
+decode = decoding
+encode = encoding
+
+# Compiling
+build = compile
+
+# Executables
+runnable = executable
+
+# Variables
+value = variable
+
+# Arguments
+arg = argument
+
+# Boolean Logic
+logic = boolean
+
+# Integers
+number = integer
+
+# Collections
+set = collection
+
+# Sequences
+order = sequence
+
+# Containers
+holder = container
+
+# Generators
+produce = generator
+
+# Coroutines
+feed = coroutine
+
+# Assignment
+set = assign
+
+# Memory Allocation
+reserve = allocate
+
+# Increment/Decrement
+add = increment
+minus = decrement
+
+# Modulo
+divide = modulo
+
+# Iteration
+repeat = iterate
+
+# Comparison
+check = compare
+
+# Hashing
+code = hash
+
+# Value Retrieval
+get = receive
+
+# Value Transmission
+send = transmit
+
+# Reassignment
+change = re-assign
+
+# Data Conversion
+convert = convert
+
+# Validation
+verify = validate
+
+# Method Overriding
+replace = override
+
+# Initialization
+begin = initialize
+
+# Data Structures
+layout = structure
+
+# Comments
+note = comment
+
+# Function Invocation
+call = invoke
+
+# Function Signatures
+id = signature
+
+# Command Line Arguments
+option = argument
+
+# Data Compression
+shrink = compress
+expand = uncompress
+
+# Encapsulation
+surround = encapsulate
+
+# Serialization
+save = serialize
+load = deserialize
+
+# Syntax
+syntax = syntax
+
+# Constraints
+limit = constraint
+
+# Directives
+rule = directive
+
+# Decorator Enhancements
+enhance = decorator
+
+# Containment
+contain = container
+
+# Encapsulation
+encap = encapsulate
+
+# Containment Operations
+contain = containment
+
+# Descending Order
+go_down = descend
+
+# Ascending Order
+go_up = ascending
+
+# Alignment
+match = align
+
+# Navigation
+travel = navigate
+
+# Branching
+fork = branch
+
+# List Concatenation
+join = concatenate
+
+# Method Overriding
+ignore = override
+
+# Code Refactoring
+redesign = refactor
+
+# Data Aggregation
+gather = aggregate
+
+# Value Mutation
+change = mutate
+
+# Mutual Exclusion
+lock = mutex
+
+# Mutator Functions
+mutate = mutator
+
+# Source Generators
+source = generator
+
+# Value Selection
+pick = select
+
+# Value Aggregation
+collect = aggregator
+
+# Event Dispatching
+send = dispatcher
+
+# Facade Pattern
+cover = façade
+
+# Enveloping
+envel = envelope
+
+# Auditing
+check = auditing
+
+# Referencing
+point = referencing
+
+# Git Operations
+reset = rebase
+switch = checkout
